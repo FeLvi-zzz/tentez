91ac2f8a33d2a0eb247b89bde05519127b777907
Soh Takada <34182539+FeLvi-zzz@users.noreply.github.com>
2022-03-05 16:43:36 +0900

    add apply test

---
+++ apply_test.go
@@ -0,0 +1,51 @@
+package tentez
+
+import (
+	"context"
+	"testing"
+
+	elbv2 "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
+)
+
+type targetsMock struct{}
+
+func (t targetsMock) targetsSlice() []Target {
+	return []Target{}
+}
+
+func (t targetsMock) fetchData(client Client) (interface{}, error) {
+	return []Target{}, nil
+}
+
+type elbv2Mock struct {
+	ModifyListenerError    error
+	ModifyRuleError        error
+	DescribeRulesError     error
+	DescribeListenersError error
+}
+
+func (m elbv2Mock) ModifyListener(ctx context.Context, params *elbv2.ModifyListenerInput, optFns ...func(*elbv2.Options)) (*elbv2.ModifyListenerOutput, error) {
+	return &elbv2.ModifyListenerOutput{}, m.ModifyListenerError
+}
+func (m elbv2Mock) ModifyRule(ctx context.Context, params *elbv2.ModifyRuleInput, optFns ...func(*elbv2.Options)) (*elbv2.ModifyRuleOutput, error) {
+	return &elbv2.ModifyRuleOutput{}, m.ModifyRuleError
+}
+func (m elbv2Mock) DescribeRules(ctx context.Context, params *elbv2.DescribeRulesInput, optFns ...func(*elbv2.Options)) (*elbv2.DescribeRulesOutput, error) {
+	return &elbv2.DescribeRulesOutput{}, m.DescribeRulesError
+}
+func (m elbv2Mock) DescribeListeners(ctx context.Context, params *elbv2.DescribeListenersInput, optFns ...func(*elbv2.Options)) (*elbv2.DescribeListenersOutput, error) {
+	return &elbv2.DescribeListenersOutput{}, m.DescribeListenersError
+}
+
+func TestExecSwitch(t *testing.T) {
+	targets := map[string]Targets{
+		"targets_mock": targetsMock{},
+	}
+	err := execSwitch(targets, Weight{}, Client{
+		elbv2: elbv2Mock{},
+	})
+
+	if err != nil {
+		t.Errorf("expected no error, but throw %v", err)
+	}
+}
